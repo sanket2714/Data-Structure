d10978d4402a080aa87864d7985a92102e092a4d
sanket2714 <44393387+sanket2714@users.noreply.github.com>
2018-10-24 15:31:26 +0530

    Implementation of Max Heap

---
+++ maxHeap.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include<climits>
+
+using namespace std;
+
+int *harr;
+int capacity;
+int heap_size;
+
+int parent(int i)
+{
+    return ((i-1)/2);
+}
+int left(int i)
+{
+    return ((2*i)+1);
+}
+int right(int i)
+{
+    return ((2*i)+2);
+}
+void maxHeap(int cap)
+{
+    heap_size=0;
+    capacity=cap;
+    harr =new int[cap];
+}
+void insertkey(int k)
+{
+    if(heap_size==capacity)
+    {
+        cout<<"\n Overflow, Could not insert a key\n";
+        return;
+    }
+    heap_size++;
+    int i= heap_size-1;
+    harr[i]=k;
+    while(i!=0 && harr[parent(i)]<harr[i])
+    {
+        swap(harr[i],harr[parent(i)]);
+        i=parent(i);
+    }
+}
+void MaxHeapify(int i)
+{
+    int l=left(i);
+    int r=right(i);
+    int largest=i;
+    if(l<heap_size && harr[l]>harr[i])
+    {
+        largest=l;
+    }
+    if(r<heap_size && harr[r]>harr[largest])
+    {
+        largest=r;
+    }
+    if(largest!=i)
+    {
+        swap(harr[i],harr[largest]);
+        MaxHeapify(largest);
+    }
+}
+int extractMax()
+{
+    if(heap_size<=0)
+    {
+        return INT_MAX;
+    }
+    if(heap_size==1)
+    {
+        heap_size--;
+        return harr[0];
+    }
+    int root=harr[0];
+    harr[0]=harr[heap_size-1];
+    heap_size--;
+    MaxHeapify(0);
+    return root;
+}
+
+void decreaseKey(int i,int new_val)
+{
+    harr[i]=new_val;
+    while(i!=0 && harr[parent(i)]<harr[i])
+    {
+        swap(harr[i],harr[parent(i)]);
+        i=parent(i);
+    }
+}
+void deletekey(int i)
+{
+    decreaseKey(i, INT_MAX);
+    extractMax();
+}
+
+int getMax()
+{
+    return (harr[0]);
+}
+
+
+/*int main()
+{
+    maxHeap(6);
+    for(int x=0; x<capacity; x++)
+    {
+        int val;
+        cin>>val;
+        insertkey(val);
+    }
+    for(int x=0; x<heap_size; x++)
+    {
+        cout<<harr[x]<<" ";
+    }
+    cout<<endl<<getMax()<<endl;
+    //cout<<extractMin()<<endl;
+    deletekey(5);
+    for(int x=0; x<heap_size; x++)
+    {
+        cout<<harr[x]<<" ";
+    }
+    return 0;
+}
+*/
