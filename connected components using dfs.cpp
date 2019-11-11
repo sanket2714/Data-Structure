3537c331acb2e5458e4fe5cbda76cd5b069dbdf3
Sanket Verma <44393387+sanket2714@users.noreply.github.com>
2019-11-11 23:41:19 +0530

    Update connected components using dfs.cpp

--- connected components using dfs.cpp
+++ connected components using dfs.cpp
@@ -1,39 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 100000;                 // Maximum number of nodes, change according to constraints.
-map<int, vector<int> > adjList;       // Considering undirected graph
-bool visited[N];
+vector<int> graph[300005];
+bool vis[300005]={false};
 
-void dfs(int u){
-  printf("%d ", u);
-  visited[u] = true;
-  for(int i = 0 ; i < adjList[u].size() ; i ++){
-    int v = adjList[u][i];          // neighbour of u
-    if(visited[v] != true){         // checking if neighbour is already visited or not
-      dfs(v);
-    }
-  }
+void dfs(int i){
+        vis[i]=true;
+        for(int j=0;j<graph[i].size();j++){
+            int ch=graph[i][j];
+            if(vis[ch]==0){
+                dfs(ch);
+            }
+        }
+        return;
 }
 
 int main(){
-  memset(visited, 0, sizeof(visited));
-  int n , edges;                    // n = number of nodes, edges = Number of edges
-  scanf("%d%d",&n,&edges);
-  for(int i = 0 ; i < edges ; i ++){
-    int u, v;
-    scanf("%d%d",&u,&v);
-    adjList[u].push_back(v);
-    adjList[v].push_back(u);  
-  }
-  int connectedComponents = 0;
-  printf("Connected Components in G are :\n");
-  for(int i = 1 ; i <= n ; i++){
-    if(!visited[i]){
-      dfs(i);
-      connectedComponents++;
-      printf("\n");
+    int vertices,edges;
+    cin>>vertices>>edges;
+    int connectedComponents=0;
+    while(e--){
+        int u,v;
+        cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
-  }
-  printf("The number of Connected Components in G is %d.\n", connectedComponents);
+    for(int i=1;i<=vertices;i++){
+        flag=0;
+        if(!vis[i]){
+            dfs(i);
+            connectedComponents++;
+        }
+    }
+    cout<<connectedComponents<<endl;
+    return 0;
 }
